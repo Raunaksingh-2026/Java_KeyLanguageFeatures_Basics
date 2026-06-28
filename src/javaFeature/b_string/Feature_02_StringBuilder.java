e33449b0b430c43f5311c211ab9fa2eb0d9cb47f
Raunaksingh-2026 <rajputraunaksingh23082005@gmail.com>
2026-06-28 23:21:58 +0530

    Modified Feature_02_StringBuilder.java File

--- src/javaFeature/b_string/Feature_02_StringBuilder.java
+++ src/javaFeature/b_string/Feature_02_StringBuilder.java
@@ -34,55 +34,61 @@ public class Feature_02_StringBuilder {
         stringBuilder.append("Banana ");
 
         /***************** Printing StringBuilder ********************/
-        System.out.println(stringBuilder);
+        System.out.println("Its STRING BUILDER -> " + stringBuilder);
 
         /***************** Creating String by converting StringBuilder to String ********************/
 ///        STRING BUILDER → STRING
-//        toString() -> Converts mutable StringBuilder --> into immutable String.
+        System.out.println("Converting STRING BUILDER → STRING ");
+
+///        toString() -> Converts mutable StringBuilder --> into immutable String.
         String stringBuilderString = stringBuilder.toString();
-        System.out.println(stringBuilderString);
+        System.out.println("Its STRING Now -> " + stringBuilderString);
+        System.out.println();
 
         /***************** Modifying String and StringBuilder ********************/
-//        StringBuilder -> Same object gets modified.
+///        StringBuilder -> Same object gets modified.
+        System.out.println("Apply Append Methods on StringBuilder");
         stringBuilder.append("Apple ");
 
-//        String -> New object gets created.
+        System.out.println("No Method like Append Available");
+///        String -> New object gets created.
         stringBuilderString = "Apple ";
 
         /***************** Reprinting String and StringBuilder ********************/
-        System.out.println(stringBuilderString);/// new String object is created
-        System.out.println(stringBuilder);/// same String object is updated
+        System.out.println("Its STRING BUILDER -> " + stringBuilder); /// same String object is updated
+        System.out.println("Its STRING -> " + stringBuilderString); /// new String object is created
+        System.out.println();
 
         /***************** Some StringBuilder method ********************/
 
 //        append() -> Can append --> int, double, char, boolean, String, Object
+        System.out.println("Appending Integer Value  : 43");
         stringBuilder.append(43); // we can add integer value
+
+        System.out.println("Appending Boolean Value  : True");
         stringBuilder.append(" " + true); // we can add boolean value
-        System.out.println(stringBuilder);
+        System.out.println("New String Builder       : " + stringBuilder + "\n");
 
-        System.out.println("Insert \"आम \" at INDEX 6" );
+        System.out.println("Insert \"आम \" at INDEX   : 6" );
         stringBuilder.insert(6, "आम ");
-        System.out.println(stringBuilder);
+        System.out.println("New String Builder       : " + stringBuilder + "\n");
 
-        System.out.println("Replacing value of ");
+        System.out.println("Replacing value of       : ");
 
-        System.out.println(stringBuilder);
+        System.out.println("New String Builder       : " + stringBuilder + "\n");
 
-        System.out.println("Delete Character Present From 5 to 8 : " );
+        System.out.println("Delete Character At      : 5 to 8 " );
         stringBuilder.delete(5, 8);
-        System.out.println(stringBuilder);
+        System.out.println("New String Builder       : " + stringBuilder + "\n");
 
-
-        System.out.println("Length of stringBuilder : " + stringBuilder.length()); // to get length of String
+        System.out.println("Length of stringBuilder   : " + stringBuilder.length()); /// to get length of String
 ///        Default Capacity = 16
 ///        Formula -> New Capacity = (oldCapacity × 2) + 2
-        System.out.println("Capacity of stringBuilder : " + stringBuilder.capacity()); // to get length of String
+        System.out.println("Capacity of stringBuilder : " + stringBuilder.capacity()); /// to get Size of Current Buffer
 
-        System.out.println("Index of \"ple\" : " +stringBuilder.indexOf("ple")); // to get index of argument
-        System.out.println("Character Present at INDEX 6 : " + stringBuilder.charAt(6)); // Return character at given index
+        System.out.println("Index of \"ple\"            : " +stringBuilder.indexOf("ple")); // to get index of argument
+        System.out.println("Character at INDEX 6      : " + stringBuilder.charAt(6)); // Return character at given index
 
-//        System.out.println(stringBuilder.reverse()); // we can reverse the given String
-        //
-
+///        System.out.println(stringBuilder.reverse()); // we can reverse the given String
     }
 }
