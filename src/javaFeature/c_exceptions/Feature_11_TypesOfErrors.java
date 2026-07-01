c2a6c668222bde7366ed3964b3e67f7569e7642f
Raunaksingh-2026 <rajputraunaksingh23082005@gmail.com>
2026-07-01 15:31:40 +0530

    Updated Feature_11_TypesOfErrors.java File.

--- src/javaFeature/c_exceptions/Feature_11_TypesOfErrors.java
+++ src/javaFeature/c_exceptions/Feature_11_TypesOfErrors.java
@@ -8,21 +8,24 @@ Java programs can encounter three major categories of errors:
     * 2. Runtime Errors (Exceptions)
         -> Occur while the program is running.
         -> Handled using Exception Handling.
+        ✔ Detected by JVM
+        ✔ Program starts but crashes
 
     3. Logical Errors
         -> Program compiles and runs successfully, --> but produces incorrect output or behaves unexpectedly.
+        ✔ Compiler finds nothing
+        ✔ JVM finds nothing
 */
 
 public class Feature_11_TypesOfErrors {
     public static void main(String[] args) {
 
         /*+++++++++++++++ COMPILE TIME ERROR +++++++++++++++++++++++*/
+///        Compiler prevents the program from running -> until these errors are corrected.
 
 //        int A = 39  // here there is missing of SEMICOLON can cause SYNTAX ERROR
 //        B = 34;  // here the VARIABLE is Not DECLARED which cause " cannot find symbol "
 //        System.out.printlin("Hello"); // Wrong Method Name
-
-///        Compiler prevents the program from running -> until these errors are corrected.
 
         /*+++++++++++++++ RUN TIME ERRORs (EXCEPTION) +++++++++++++++++++++++*/
 ///        Program compiles successfully. --> Exception occurs while execution.
@@ -46,5 +49,18 @@ public class Feature_11_TypesOfErrors {
 //            -> ArrayIndexOutOfBoundsException
 
         /*+++++++++++++++ LOGICAL ERROR +++++++++++++++++++++++*/
+///        Program compiles ✔ -> Program runs ✔ --> Output is Wrong ✘
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a - b);
+///        Expected: Addition
+///        Actual: Subtraction
+
+        int i = 1;
+        while (i > 0) {  ///  there is no any terminating condition which leads to INFINITE loop
+            System.out.println(i);
+            i++;
+        }
     }
 }
